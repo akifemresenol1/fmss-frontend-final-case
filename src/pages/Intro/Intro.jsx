15a955f31458bd442d52cf22badeaa032c8e1bdb
Akif Emre Senol <akifffemreee@gmail.com>
2023-04-27 15:33:15 +0300

    content added and edited on the intro page

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -9,7 +9,28 @@ function Intro() {
   const navigate = useNavigate();
   return (
     <div className="intro">
-      <img className="logo" src={logo} alt="logo" />
+      <img className="logo-intro" src={logo} alt="logo" />
+
+      <div className="scroller">
+        <div id="intro-content">
+          {" "}
+          <h2 className="title"> It is a dark time for the Rebellion. </h2>
+          <p>Although the Death Star has been </p>
+          <p>destroyed,</p>
+          <p>Imperial troops have driven the Rebel</p>
+          <p>forces from their hidden base</p>
+          <p>and pursued them across the galaxy.</p>
+          <p>Evading the dreaded Imperial Starfleet, </p>
+          <p>a group of freedom fighters led by </p>
+          <p>Luke Skywalker</p>
+          <p>has established a new secret base on the</p>
+          <p>remote ice world of Hoth.</p>
+          <p>The evil lord Darth Vader,</p>
+          <p> obsessed with finding young Skywalker,</p>
+          <p>has dispatched thousands of remote</p>
+          <p>probes into the far reaches of space..</p>
+        </div>
+      </div>
       <Speaker src={audio} controls autoplay />
       <button
         className="explore-button"
